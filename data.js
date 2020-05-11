e6451baad0d78dece55daff96422720bfcaa2d86
Lucas Dos Santos <lucas.nunes.santoss@gmail.com>
2020-05-11 20:15:52 -0300

    alterado dados dinâmicos, criada página de erro 404, e adicionada página de execução de vídeo.

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+    id:"A505y0Pmn2Y",
+    title:"Batida Perfeita",
+    duration:"Duração: 4:02 min",
+    vocal:"Vozes: Kuba, Dellas",
+    featured: true
+  },
+  {
+    id:"9Iz7A5KE7MU",
+    title:"Pra me Provocar",
+    duration:"Duração: 4:07 min",
+    vocal:"Vozes: Kuba, Dellas, Black Z"
+  },
+  {
+    id:"l2e94KQMNzo",
+    title:"No Plano Perfeito [Stúdio]",
+    duration:"Duração: 4:48 min",
+    vocal:"Vozes: Kuba, Dellas, Black Z",
+    featured: true
+  },
+  {
+    id:"mtAyblAqpH8",
+    title:"Swingueira",
+    duration:"Duração: 4:14 min",
+    vocal:"Vozes: Kuba, Dellas, Black Z, Dom Armanni",
+    featured: true
+  },
+  {
+    id:"z8n47xQOMvE",
+    title:"Minha vida, minha vibe",
+    duration:"Duração: 3:59 min",
+    vocal:"Vozes: Kuba, Black Z"
+  },
+  {
+    id:"VuB-OXBIkkk",
+    title:"Nasci pra vencer",
+    duration:"Duração: 3:01 min",
+    vocal:"Vozes: Black Z"
+  }
+]
